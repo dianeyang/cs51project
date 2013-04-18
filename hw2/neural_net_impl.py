9250c8ac3e2e6007123b45e7e643ce76db8c73ab
Lisa Wang <lisawang01@college.harvard.edu>
2013-04-17 21:55:53 -0400

    fix shit again

--- hw2/neural_net_impl.py
+++ hw2/neural_net_impl.py
@@ -312,14 +312,13 @@ class SimpleNetwork(EncodedNetworkFramework):
     # 2) Add an output node for each possible digit label.
     inputNodes=[]
     outputNodes=[]
-    framework = EncodedNetworkFramework()
     for i in range(195)
       inputNodes[i] = Node()
-      framework.network.AddNode(inputNodes[i], 1)
+      self.network.AddNode(inputNodes[i], 1)
     for j in range(9)
       outputNodes[j] = Node()
-      framework.network.AddNode(outputNodes[j], 3)
-    framework.InitializeWeights
+      self.network.AddNode(outputNodes[j], 3)
+   
  
     
     
