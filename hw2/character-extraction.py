242fbbcc15e1f3ba11d38e92fb397461e0930019
Diane Yang <dianeyang0@gmail.com>
2013-04-30 14:12:20 -0400

    added lotsa comments

--- hw2/character-extraction.py
+++ hw2/character-extraction.py
@@ -34,8 +34,11 @@ class ProcessedImage(object):
                     return False
             return True
     
+        # store lower & upper bounds for each line of text
         lower, upper = None, None
+        # keep track of whether previous row was blank
         prev_blank = True
+        # list of images lines of text
         lines = []
         
         for y in range(self.height):
@@ -79,27 +82,42 @@ class ProcessedImage(object):
         
         # splits a line of text into separate characters
         def split_line(line):
+            # stores left & right bounds of each char
             left, right = None, None
             width, height = line.size
+            # counter for number of blank columns - help identify spaces
             blanks = 0
+            # remember whether previous column was blank
             prev_blank = True
+            # remember whether at start of new line
+            # (so we don't add an unnecessary space)
             new_line = True
+            # collect list of images of chars
             chars = []
+
             for x in range(width):
+                # if we go from having non-blank columns to blank columns,
+                # then we have found the right side of a char
                 if col_blank(x, height, line.load()):
                     if not prev_blank:
                         right = x
                     prev_blank = True
                     blanks += 1
                 else:
+                    # if we go from having blank columns to non-blank columns,
+                    # then we have found the left side of a char
                     if prev_blank:
                         left = x
                         copied = False
                     prev_blank = False
+                    # if we've seen height/5 consecutive blank columns,
+                    # then this must be a space
                     if blanks >= height/5 and not new_line and not space_added:
                         chars.append(self.space)
                         space_added = True
                     blanks = 0
+                # if we've found the left & right boundaries of a new char,
+                # copy it and add that to our list
                 if right > left and not copied:
                     chars.append(copy(left, 0, right, height))
                     copied = True
@@ -129,19 +147,25 @@ class ProcessedImage(object):
         for x in chars:
         	newsize = float(self.new_size)
         	old_w, old_h = x.size
+
+            # if landscape, set width to newsize
         	if old_w > old_h:
         		newwidth = self.new_size
         		newheight = int(old_h * (newsize / old_w))
         		new = x.resize((newwidth, newheight), Image.ANTIALIAS)
+            # if portrait (or square) set height to newsize
         	else:
         		newwidth = int(old_w * (newsize / old_h))
         		newheight = self.new_size
         		new = x.resize((newwidth, newheight), Image.ANTIALIAS)
+            # create square white canvas
         	newImage = Image.new('1', size = (self.new_size, self.new_size), color=255)
+            # paste resized char into middle of canvas
         	if old_w > old_h:
         		newImage.paste(new, (0, int((newsize-newheight) / 2)))
         	else:
         		newImage.paste(new, (int((newsize - newwidth) / 2), 0))
+            # add to list of resized chars
         	resizedchars.append(newImage)
         return resizedchars
         
