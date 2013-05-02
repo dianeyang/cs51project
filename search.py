e6af92d43249292857f8b380513573b957e87b88
Kim Soffen <Kim@Kims-MacBook-Pro-2.local>
2013-05-01 22:26:57 -0400

    changed search

--- search.py
+++ search.py
@@ -2,29 +2,36 @@ import sys
 
 def main():
 
-  # get query from command line args
-  query = ""
-  for i in range(len(sys.argv)):
-    if i > 0:
-      query += sys.argv[i]
-      query += " "
+  # check if a query was entered
+  if len(sys.argv) >= 2:
 
-  # get contents of searchable file
-  search_file = open('searchable.txt', "r")
-  contents = search_file.read()
-  search_file.close()
+      # get query from command line args
+	  query = ""
+	  for i in range(len(sys.argv)):
+	    if i == 1:
+	      query += sys.argv[1]
+	    if i > 1:
+	      query += " "
+	      query += sys.argv[i]
 
-  # make search case insensitive
-  contents = contents.lower()
-  query = query.lower()
+	  # get contents of searchable file
+	  search_file = open('searchable.txt', "r")
+	  contents = search_file.read()
+	  search_file.close()
 
-  # print out the location of the query
-  index = contents.find(query)
-  if index >= 0:
-  	print "Query found at position %d" % index
+	  # make search case insensitive
+	  contents = contents.lower()
+	  query = query.lower()
+
+	  # print out the location of the query
+	  index = contents.find(query)
+	  if index >= 0:
+	  	print "Query found at position %d" % index
+	  else:
+	    print "Query not found"
+  
   else:
-    print "Query not found"
-
+  	print "You must submit a search query"
 
 if __name__ == "__main__":
   main()
